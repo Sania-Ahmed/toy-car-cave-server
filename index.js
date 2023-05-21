80ba2f19cb7bb257c10d6c13b72b424fabaf3432
SaniaAhmed1 <tasminsania77@gmail.com>
2023-05-21 06:25:04 -0700

    done

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ app.use(cors());
 app.use(express.json());
 
 
-
+// mongo setup
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.74wxpsk.mongodb.net/?retryWrites=true&w=majority`;
 
 
