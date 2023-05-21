3077b9b283adcb0024cff1df9d742a1cba0fc05a
SaniaAhmed1 <tasminsania77@gmail.com>
2023-05-21 04:25:52 -0700

    vercel data issue fixed

--- index.js
+++ index.js
@@ -27,8 +27,7 @@ const client = new MongoClient(uri, {
 async function run() {
   try {
    
-    // Send a ping to confirm a successful connection
-    await client.db("admin").command({ ping: 1 });
+    
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
 
     const database = client.db("toyCarDB");
